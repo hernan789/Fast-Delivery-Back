3ffabeb357b9666195cf255d69ca8ade0bb22cd2
hernan789 <hernanlduarte@hotmail.com>
2024-02-21 15:14:39 -0300

    update login & add auth

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -1,11 +1,8 @@
-const { Request, Response } = require("express");
 import db from "../src/config";
 import User from "../src/models/User";
-import userController from "../src/controllers/userControllers";
-import { jest, describe, expect, beforeEach, test } from "@jest/globals";
-import { Request, Response } from "express";
-
-import { generateToken } from "../src/config/token.ts";
+import { describe, expect, beforeEach, test } from "@jest/globals";
+import request from "supertest";
+import app from "../server.ts";
 import {
   LoginRequestBody,
   CreateUserRequestBody,
@@ -15,86 +12,63 @@ describe("userController", () => {
   describe("login", () => {
     beforeAll(async () => {
       await db.validate();
-      jest.mock("../src/models/User");
     });
 
     afterAll(async () => {
-      await User.destroy({ where: {} });
-
       await db.close();
-      jest.clearAllMocks();
     });
-    const setupReqRes = (reqBody: any) => {
-      const req: Partial<Request> = { body: reqBody };
-      const res = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      } as unknown as Response;
-      return { req, res };
-    };
-    it("if there is no email return 400", async () => {
-      const { req, res } = setupReqRes({ password: "password" });
 
-      await userController.login(req as Request, res);
+    it("should return 400 if there is no", async () => {
+      const res = await request(app)
+        .post("/api/users/login")
+        .send({ password: "password" });
 
-      expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.json).toHaveBeenCalledWith({
-        message: "Email no proporcionado.",
-      });
+      expect(res.status).toBe(400);
+      expect(res.body).toEqual({ message: "Email no proporcionado." });
     });
+
     it("should return 400 if email format is invalid", async () => {
-      const { req, res } = setupReqRes({ email: "password" });
+      const res = await request(app)
+        .post("/api/users/login")
+        .send({ email: "password" });
 
-      await userController.login(req as Request, res);
-
-      expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.json).toHaveBeenCalledWith({
+      expect(res.status).toBe(400);
+      expect(res.body).toEqual({
         message: "El formato de correo electrónico es inválido.",
       });
     });
+
     it("should return 400 if no password is provided", async () => {
-      const { req, res } = setupReqRes({ email: "hola@gmail.com" });
+      const res = await request(app)
+        .post("/api/users/login")
+        .send({ email: "hola@gmail.com" });
 
-      await userController.login(req as Request, res);
-
-      expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.json).toHaveBeenCalledWith({
-        message: "Contraseña no proporcionada.",
-      });
+      expect(res.status).toBe(400);
+      expect(res.body).toEqual({ message: "Contraseña no proporcionada." });
     });
+
     it("should return 400 if no user was found", async () => {
       const testUser = {
         email: "garmando@gmail.com",
         password: "otroletravaladna",
       };
-      const { req, res } = setupReqRes(testUser);
 
-      await userController.login(req as Request, res);
+      const res = await request(app).post("/api/users/login").send(testUser);
 
-      expect(res.status).toHaveBeenCalledWith(400);
-      expect(res.json).toHaveBeenCalledWith({
-        error: "Usuario no encontrado.",
-      });
+      expect(res.status).toBe(400);
+      expect(res.body).toEqual({ error: "Usuario no encontrado." });
     });
-    it("should return a status 200 if the login was successfull", async () => {
+
+    it("should return a status 200 if the login was successful", async () => {
       const testUser = {
         email: "hernanduarte@gmail.com",
         password: "Hernan1234",
       };
 
-      const req: Partial<Request> = { body: testUser };
-      const res = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-        cookie: jest.fn(),
-      } as unknown as Response;
+      const res = await request(app).post("/api/users/login").send(testUser);
 
-      await userController.login(req as Request, res);
-      // expect(res.cookie).toHaveBeenCalled();
-      expect(res.status).toHaveBeenCalledWith(200);
-      expect(res.json).toHaveBeenCalledWith({
-        message: "Usuario logeado con éxito.",
-      });
+      expect(res.status).toBe(200);
+      expect(res.body).toEqual({ message: "Usuario logeado con éxito." });
     });
   });
 });
